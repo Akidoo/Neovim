a449ab385759817f59a17a3542d26d9638c043db
Akidoo <plunkels@gmail.com>
2024-09-13 19:49:46 +0800

    update plugins

--- init.lua
+++ init.lua
@@ -21,10 +21,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
